b8b99b2658d5033cef933045e9246e171a94818a
haralearati72-tech <haralearati72@gmail.com>
2026-04-17 20:45:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (12.21%) - LeetSync

---
+++ 3371-harshad-number/harshad-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        
+        int sum = 0;
+        int n = x;   // copy original number
+        
+        while(n > 0)
+        {
+            int rem = n % 10;
+            sum = sum + rem;
+            n = n / 10;
+        }
+        
+        if(x % sum == 0)
+        {
+            return sum;
+        }
+        
+        return -1;
+    }
+}
