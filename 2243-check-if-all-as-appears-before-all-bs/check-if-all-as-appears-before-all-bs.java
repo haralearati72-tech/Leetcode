df183eaa5e4190c7182af6351ca5aefb613bdb33
haralearati72-tech <haralearati72@gmail.com>
2026-07-23 14:54:24 +0530

    Time: 1 ms (16.31%) | Memory: 42.9 MB (42.77%) - LeetSync

---
+++ 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkString(String s) {
+        boolean flag = true;
+        for(int i = 0; i<s.length()-1; i++)
+        {
+            
+            if(s.charAt(i) == 'b' && s.charAt(i+1) =='a')
+            {
+                flag = false;
+            }
+           
+        }
+        if(flag==true)
+        {
+          return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
