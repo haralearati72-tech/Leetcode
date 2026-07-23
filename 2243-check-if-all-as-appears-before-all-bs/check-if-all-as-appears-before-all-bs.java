cf907d3ed0692f53760066bd8fedc9e5e5647829
haralearati72-tech <haralearati72@gmail.com>
2026-07-23 14:57:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (78.33%) - LeetSync

--- 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.java
+++ 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.java
@@ -1,15 +1,12 @@
 class Solution {
     public boolean checkString(String s) {
-     
-        for(int i = 0; i<s.length()-1; i++)
-        {
-            
+        
             if(s.contains("ba"))
             {
                 return false;
             }
            
-        }
+        
        return true;
     }
 }
