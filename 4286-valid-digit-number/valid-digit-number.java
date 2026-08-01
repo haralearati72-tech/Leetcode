49bdbf207eff632835b6ce9d68af6bd8bb5e5b4a
haralearati72-tech <haralearati72@gmail.com>
2026-08-01 13:10:40 +0530

    Time: 1 ms (98.35%) | Memory: 42.5 MB (66.00%) - LeetSync

---
+++ 4286-valid-digit-number/valid-digit-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean validDigit(int n, int x) {
+       
+       boolean hasX = false;
+        while(n>9)
+        {
+             if(n%10 == x)
+             {
+                hasX = true;
+             }
+              n = n/10;
+        }
+        return hasX && (n!=x);   
+    }
+}
