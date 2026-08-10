6f947a918b375ead4768dfb8348e89995a47f816
haralearati72-tech <haralearati72@gmail.com>
2026-08-10 13:33:53 +0530

    Time: 1 ms (83.61%) | Memory: 42.8 MB (56.08%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        
+        String[] words = s.split(" ");
+         if(pattern.length() !=words.length)
+         {
+            return false;
+         }
+        HashMap<Character,String> map = new HashMap<>();
+       
+
+            for(int i = 0; i<pattern.length(); i++)
+            {
+                char ch = pattern.charAt(i);
+                if(map.containsKey(ch))
+                {
+                    String value = map.get(ch);
+                    if(value.equals(words[i]) == false)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                     if (map.containsValue(words[i])) {
+                    return false;
+                }
+
+                    map.put(ch,words[i]);
+                }
+            }
+            return true;
+    }
+}
